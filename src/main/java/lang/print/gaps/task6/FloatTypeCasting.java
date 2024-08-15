92e311431236fcc348786d8de466625aec195498
litim smail <litimsmail@gmail.com>
2024-08-15 12:40:20 +0100

    Task 6

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int roundIntoInteger = Math.round(numberToBeRounded);
+        System.out.println(roundIntoInteger);
+    }
+    public  static void main (String [] args){
+        FloatTypeCasting roundNbr = new FloatTypeCasting();
+        roundNbr.roundNumber(3.6f);
+
     }
 }
